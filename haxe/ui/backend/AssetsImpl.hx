11e041af22b313d14c9ffc9e46a89f933845c4e1
Riley Dutton <rgd@hey.com>
2025-04-13 09:08:46 -0500

    add support for loading from autopak

--- haxe/ui/backend/AssetsImpl.hx
+++ haxe/ui/backend/AssetsImpl.hx
@@ -12,28 +12,38 @@ class AssetsImpl extends AssetsBase {
     }
 
     private override function getImageInternal(resourceId:String, callback:haxe.ui.assets.ImageInfo->Void) {
-        try {
-            var loader:hxd.res.Loader = hxd.Res.loader;
-            if (loader != null) {
-                if (loader.exists(resourceId)) {
-                    var image:Image = loader.load(resourceId).toImage();
-                    var size:Dynamic = image.getSize();
-                    var imageInfo:haxe.ui.assets.ImageInfo = {
-                        width: size.width,
-                        height: size.height,
-                        data: image.toBitmap()
-                    };
-                    callback(imageInfo);
-                } else {
-                    callback(null);
-                }
-            } else {
-                callback(null);
-            }
-        } catch (e:Dynamic) {
-            trace(e);
-            callback(null);
-        }
+        // try {
+        //     var loader:hxd.res.Loader = hxd.Res.loader;
+        //     if (loader != null) {
+        //         if (loader.exists(resourceId)) {
+        //             var image:Image = loader.load(resourceId).toImage();
+        //             var size:Dynamic = image.getSize();
+        //             var imageInfo:haxe.ui.assets.ImageInfo = {
+        //                 width: size.width,
+        //                 height: size.height,
+        //                 data: image.toBitmap()
+        //             };
+        //             callback(imageInfo);
+        //         } else {
+        //             callback(null);
+        //         }
+        //     } else {
+        //         callback(null);
+        //     }
+        // } catch (e:Dynamic) {
+        //     trace(e);
+        //     callback(null);
+        // }
+        js.html.Console.log("TRYING TO LOAD RESOURCE ID " + resourceId);
+        tools.AutoPak.loadImageForUi(resourceId, (loadedTile) -> {
+           var imageInfo:haxe.ui.assets.ImageInfo = {
+                width: loadedTile.width,
+                height: loadedTile.height,
+                data: loadedTile
+           };
+           js.html.Console.log(loadedTile);
+           callback(imageInfo);
+        });
     }
 
     private override function getImageFromHaxeResource(resourceId:String, callback:String->haxe.ui.assets.ImageInfo->Void) {
